3e52f253c6410b589ee83839f901f916bf0efb87
kylem038 <misenckj@gmail.com>
2017-01-10 10:51:27 -0700

    Updating state from inputs

--- mod-4-final/src/components/Form.js
+++ mod-4-final/src/components/Form.js
@@ -10,23 +10,32 @@ class Form extends Component {
     };
   }
 
+  updateProperties(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
   render() {
     return (
       <div className="FormWrapper">
         <form className="Form">
           <input
             className='grudge-name-input'
+            name='name'
             placeholder='Name of your grudge'
             type='text'
             aria-label='Name of your grudge'
             value={this.state.name}
+            onChange={(e) => this.updateProperties(e)}
           />
           <textarea
             className='motivation-input'
+            name='motivation'
             placeholder='Why do you despise this person?'
             type='text'
             aria-label='Why you despise said person'
             value={this.state.motivation}
+            onChange={(e) => this.updateProperties(e)}
           />
           <button type='submit'>Submit</button>
         </form>
