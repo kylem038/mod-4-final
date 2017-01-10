1e5a23a6ccb22b4d7d8332fef422ae6fd0ae8e2b
kylem038 <misenckj@gmail.com>
2017-01-10 10:00:07 -0700

    Added default data to server

--- mod-4-final/server.js
+++ mod-4-final/server.js
@@ -4,6 +4,16 @@ const app = express();
 app.set('port', process.env.PORT || 3001);
 app.locals.title = 'Grudge Bin';
 
+app.locals.db = {
+  grudges: {
+    data: [
+      {id:1, name:'Justin', offence:'Being a douche', forgiven: false},
+      {id:2, name:'That guy', offence:'Not cleaning', forgiven: true},
+      {id:3, name:'Kreston', offence:'Also being a douche', forgiven: false},
+    ]
+  }
+};
+
 app.get('/', (request, response) => {
   response.send('Hello World!');
 });
